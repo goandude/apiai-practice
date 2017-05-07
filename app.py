9d7bfbec3529d25fa3bf86763e776e3de3eb00e9
goandude <goandude@gmail.com>
2017-05-07 00:07:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -60,11 +60,13 @@ def pr(req):
     query = parameters.get("q")
     results = wiki.find(query) 
     article = wiki.get_article(results[0])
-    query = article.summary 
+    result = article.summary 
+    result1 = duckduckgo.get_zci(query)
+    
     return {
-        "speech": query,
+        "speech": result,
         "displayText": query,
-        # "data": data,
+        "data": result1,
         # "contextOut": [],
         "source": "apiai-weather-webh29ook-sample"
     }    
